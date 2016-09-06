4737fc9d6c528645ea5ea0145dca769b05afdd0e
Peter Hsu <sysu.peter.hsu@gmail.com>
2016-09-06 16:08:42 +0800

    first commit

---
+++ README.js
@@ -0,0 +1,12 @@
+
+Steps:
+
+1. npm install --save-dev webdriverio
+
+2. npm install -g selenium-standalone
+
+3. selenium-standalone install
+
+4. selenium-standalone start
+
+5. node your_test_js.js
